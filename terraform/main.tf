327a7a61fab9f74b62e38144fe74057d5effffc0
A. Maury <aurelien.maury@wescale.fr>
2017-05-18 11:18:07 +0200

    landscape

---
+++ terraform/main.tf
@@ -0,0 +1,174 @@
+provider "aws" {}
+
+data "aws_ami" "debian" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["debian-jessie-amd64-hvm-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["379101102735"] # Debian Project
+}
+
+
+variable "vpc_cidr" {}
+
+variable "vpc_name" {}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "${var.vpc_cidr}"
+
+  enable_dns_hostnames = true
+
+  tags {
+    Name = "${var.vpc_name}"
+  }
+}
+
+resource "aws_key_pair" "bastion_keypair" {
+  key_name   = "${var.vpc_name}-bastion"
+  public_key = "${var.bastion_default_public_key}"
+}
+
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = "${aws_vpc.vpc.id}"
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gateway.id}"
+  }
+  tags {
+    Name = "Main route table for ${var.vpc_name} VPC"
+  }
+}
+
+resource "aws_main_route_table_association" "main" {
+  route_table_id = "${aws_route_table.main.id}"
+  vpc_id = "${aws_vpc.vpc.id}"
+}
+
+module "zone_a" {
+  source = "github.com/aurelienmaury/tf-mod-az-plus-nat"
+  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_name = "${var.vpc_name}"
+  availability_zone = "eu-west-1a"
+  public_subnet_cidr = "${var.public_subnet_cidr_a}"
+  private_subnet_cidr = "${var.private_subnet_cidr_a}"
+  public_gateway_route_table_id = "${aws_route_table.main.id}"
+  bastion_default_public_key = "${var.bastion_default_public_key}"
+}
+
+module "zone_b" {
+  source = "github.com/aurelienmaury/tf-mod-az-plus-nat"
+  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_name = "${var.vpc_name}"
+  availability_zone = "eu-west-1b"
+  public_subnet_cidr = "${var.public_subnet_cidr_b}"
+  private_subnet_cidr = "${var.private_subnet_cidr_b}"
+  public_gateway_route_table_id = "${aws_route_table.main.id}"
+  bastion_default_public_key = "${var.bastion_default_public_key}"
+}
+
+module "zone_c" {
+  source = "github.com/aurelienmaury/tf-mod-az-plus-nat"
+  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_name = "${var.vpc_name}"
+  availability_zone = "eu-west-1c"
+  public_subnet_cidr = "${var.public_subnet_cidr_c}"
+  private_subnet_cidr = "${var.private_subnet_cidr_c}"
+  public_gateway_route_table_id = "${aws_route_table.main.id}"
+  bastion_default_public_key = "${var.bastion_default_public_key}"
+}
+
+resource "aws_security_group" "bastion_realm" {
+
+  name_prefix = "${var.vpc_name}-bastion-realm"
+
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "TCP"
+    security_groups = [
+      "${module.zone_a.bastion_sg}",
+      "${module.zone_b.bastion_sg}",
+      "${module.zone_c.bastion_sg}"
+    ]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+
+data "template_file" "inventory" {
+  template = "${file("${path.module}/templates/inventory.tpl")}"
+
+  vars {
+    bastion_a = "${module.zone_a.bastion_ip}"
+    bastion_b = "${module.zone_b.bastion_ip}"
+    bastion_c = "${module.zone_c.bastion_ip}"
+  }
+}
+
+resource "null_resource" "inventories" {
+
+  triggers {
+    bastion_a = "${module.zone_a.bastion_ip}"
+    bastion_b = "${module.zone_b.bastion_ip}"
+    bastion_c = "${module.zone_c.bastion_ip}"
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${data.template_file.inventory.rendered}' > ${path.module}/../inventories/bastions"
+  }
+
+}
+resource "aws_instance" "hidden" {
+
+  ami = "${data.aws_ami.debian.id}"
+  instance_type = "t2.micro"
+  key_name = "${aws_key_pair.bastion_keypair.key_name}"
+
+  subnet_id = "${module.zone_a.private_subnet_id}"
+
+  vpc_security_group_ids = [
+    "${aws_security_group.bastion_realm.id}"
+  ]
+
+  tags {
+    Name = "Hidden ${var.vpc_name}"
+  }
+}
+//
+//resource "aws_instance" "hidden_2" {
+//
+//  ami = "${data.aws_ami.debian.id}"
+//  instance_type = "t2.micro"
+//  key_name = "${aws_key_pair.bastion_keypair.key_name}"
+//
+//  subnet_id = "${module.zone_c.public_subnet_id}"
+//
+//  vpc_security_group_ids = [
+//    "${aws_security_group.bastion_realm.id}",
+//    "${aws_security_group.orgie.id}"
+//  ]
+//
+//  tags {
+//    Name = "exposed ${var.vpc_name}"
+//  }
+//}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,14 @@
+vpc_name = "kubic-aws"
+
+vpc_cidr = "10.42.0.0/16"
+
+public_subnet_cidr_a = "10.42.10.0/24"
+private_subnet_cidr_a = "10.42.110.0/24"
+
+public_subnet_cidr_b = "10.42.20.0/24"
+private_subnet_cidr_b = "10.42.120.0/24"
+
+public_subnet_cidr_c = "10.42.30.0/24"
+private_subnet_cidr_c = "10.42.130.0/24"
+
+bastion_default_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6XxpygH3MaBVs+cC5vnjCgWPlkHX1JkvfZ7JmXMQkEPpNb52csjYaa0/cQ7OZySpY1pwai31kVJfooPkd1dIJJ2TFlkV4wVCJMlpkmJcvyJ5amGmhJMjK0mHRd8LP35ercnOOlDUQbrVAgZMRIst4fcLYZ5Wp+voCHTkcuP0FdLZk8ve5GiIMws3Nnd7ww/69skt2/nvs8kKYAti2u1HSpOAWY5tikPbV6+FcNQIVdGcJkrVLBggAgM29XrOQhfLCDiLAV/FSHeK4ss53+L4fDfBEeVKlRSNh1ZnEBYM8lVwo9xtEXg2jMKlth93c1crQYLPtl+lcWI0tG7qUgj1H amaury@MacBook-Pro-de-Aurelien.local"
