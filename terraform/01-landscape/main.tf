40df67ae9756cba9814a39dd42581d9d7c4958a2
A. Maury <aurelien.maury@wescale.fr>
2017-06-01 10:10:44 +0200

    prometheus-consul up and running

--- terraform/01-landscape/main.tf
+++ terraform/01-landscape/main.tf
@@ -136,6 +136,13 @@ resource "aws_security_group" "server" {
     cidr_blocks     = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 9100
+    to_port = 9110
+    protocol = "TCP"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port       = 0
     to_port         = 0
