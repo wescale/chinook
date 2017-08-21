441ce2a4574985de6be6e9720112badb8bac772b
A. Maury <aurelien.maury@wescale.fr>
2017-08-21 17:44:17 +0200

    oh la belle prise

--- terraform/logstore/logstore-realm-sg.tf
+++ terraform/logstore/logstore-realm-sg.tf
@@ -21,3 +21,12 @@ resource "aws_security_group_rule" "sgri_logstore_realm_input" {
   protocol            = "tcp"
   source_security_group_id          = "${data.terraform_remote_state.landscape.bastion_realm_sg}"
 }
+
+resource "aws_security_group_rule" "sgri_logstore_realm_graylog_inputs" {
+  security_group_id   = "${aws_security_group.logstore_realm.id}"
+  type                = "ingress"
+  from_port           = 5000
+  to_port             = 5999
+  protocol            = "tcp"
+  source_security_group_id          = "${data.terraform_remote_state.landscape.bastion_realm_sg}"
+}
