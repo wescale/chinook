8043550941b1e637c326df5d68cbf39bbeefbc72
A. Maury <aurelien.maury@wescale.fr>
2017-08-23 17:15:13 +0200

    kiki powa

--- create.sh
+++ create.sh
@@ -4,6 +4,4 @@ for step in 01-landscape 020-entry-points 021-monitor 03-masters 04-workers logs
     ansible-playbook plays/deploy-tf-layer.yml -e auto_apply=true -e layer_name=$step -vvv
 done
 
-sleep 180
-
-ansible-playbook plays/configure.yml
+date
--- terraform/logstore/logstore-realm-sg.tf
+++ terraform/logstore/logstore-realm-sg.tf
@@ -4,29 +4,20 @@ resource "aws_security_group" "logstore_realm" {
   vpc_id              = "${data.terraform_remote_state.landscape.vpc_id}"
 }
 
-resource "aws_security_group_rule" "sgri_logstore_realm_backbone" {
-  security_group_id   = "${aws_security_group.logstore_realm.id}"
-  type                = "ingress"
-  from_port           = 9300
-  to_port             = 9300
-  protocol            = "tcp"
-  source_security_group_id          = "${aws_security_group.logstore_realm.id}"
-}
-
-resource "aws_security_group_rule" "sgri_logstore_realm_input" {
-  security_group_id   = "${aws_security_group.logstore_realm.id}"
-  type                = "ingress"
-  from_port           = 9200
-  to_port             = 9200
-  protocol            = "tcp"
-  source_security_group_id          = "${data.terraform_remote_state.landscape.bastion_realm_sg}"
-}
-
 resource "aws_security_group_rule" "sgri_logstore_realm_graylog_inputs" {
   security_group_id   = "${aws_security_group.logstore_realm.id}"
   type                = "ingress"
   from_port           = 5000
   to_port             = 5999
+  protocol            = "tcp"
+  source_security_group_id          = "${data.terraform_remote_state.landscape.bastion_realm_sg}"
+}
+
+resource "aws_security_group_rule" "sgri_bastion_realm_rp" {
+  security_group_id   = "${aws_security_group.logstore_realm.id}"
+  type                = "ingress"
+  from_port           = 9000
+  to_port             = 9000
   protocol            = "tcp"
   source_security_group_id          = "${data.terraform_remote_state.landscape.bastion_realm_sg}"
 }
