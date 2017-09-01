e55605546c0d1519ae957f7aa5062f6cf7ed5dba
A. Maury <aurelien.maury@wescale.fr>
2017-09-02 01:24:11 +0200

    polish

--- create.sh
+++ create.sh
@@ -7,10 +7,13 @@ for step in 01-landscape 020-entry-points 021-monitor 03-masters 04-workers logs
 done
 
 echo "$(date) - end deploy infra" >> run.log
-sleep 420
+
+sleep 300
 
 ansible -m ping bastions
 
 echo "$(date) - start ansible" >> run.log
+
 ansible-playbook plays/configure.yml
+
 echo "$(date) - end ansible" >> run.log
